4b998ed0f64a38841e90882d9be957a77fb05238
IvanFebriansyah <87108996+IvanFebriansyah@users.noreply.github.com>
2021-10-15 01:10:28 +0700

    Add files via upload

---
+++ Praktikum2/Praktikum2/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace Praktikum2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbperempuan = new System.Windows.Forms.RadioButton();
+            this.rblaki = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.sudah = new System.Windows.Forms.RadioButton();
+            this.rbbelum = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnhasil = new System.Windows.Forms.Button();
+            this.txtanak = new System.Windows.Forms.TextBox();
+            this.txtgaji = new System.Windows.Forms.TextBox();
+            this.txthasil = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbperempuan);
+            this.groupBox1.Controls.Add(this.rblaki);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(18, 8);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(291, 39);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // rbperempuan
+            // 
+            this.rbperempuan.AutoSize = true;
+            this.rbperempuan.Location = new System.Drawing.Point(205, 14);
+            this.rbperempuan.Margin = new System.Windows.Forms.Padding(2);
+            this.rbperempuan.Name = "rbperempuan";
+            this.rbperempuan.Size = new System.Drawing.Size(79, 17);
+            this.rbperempuan.TabIndex = 2;
+            this.rbperempuan.TabStop = true;
+            this.rbperempuan.Text = "Perempuan";
+            this.rbperempuan.UseVisualStyleBackColor = true;
+            // 
+            // rblaki
+            // 
+            this.rblaki.AutoSize = true;
+            this.rblaki.Location = new System.Drawing.Point(108, 16);
+            this.rblaki.Margin = new System.Windows.Forms.Padding(2);
+            this.rblaki.Name = "rblaki";
+            this.rblaki.Size = new System.Drawing.Size(70, 17);
+            this.rblaki.TabIndex = 1;
+            this.rblaki.TabStop = true;
+            this.rblaki.Text = "Laki - laki";
+            this.rblaki.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Jenis Kelamin";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.sudah);
+            this.groupBox2.Controls.Add(this.rbbelum);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(18, 44);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Size = new System.Drawing.Size(312, 39);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // sudah
+            // 
+            this.sudah.AutoSize = true;
+            this.sudah.Location = new System.Drawing.Point(205, 16);
+            this.sudah.Margin = new System.Windows.Forms.Padding(2);
+            this.sudah.Name = "sudah";
+            this.sudah.Size = new System.Drawing.Size(99, 17);
+            this.sudah.TabIndex = 2;
+            this.sudah.TabStop = true;
+            this.sudah.Text = "Sudah menikah";
+            this.sudah.UseVisualStyleBackColor = true;
+            // 
+            // rbbelum
+            // 
+            this.rbbelum.AutoSize = true;
+            this.rbbelum.Location = new System.Drawing.Point(108, 16);
+            this.rbbelum.Margin = new System.Windows.Forms.Padding(2);
+            this.rbbelum.Name = "rbbelum";
+            this.rbbelum.Size = new System.Drawing.Size(97, 17);
+            this.rbbelum.TabIndex = 1;
+            this.rbbelum.TabStop = true;
+            this.rbbelum.Text = "Belum menikah";
+            this.rbbelum.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 17);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Status Pernikahan";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 99);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Jumlah Anak";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 136);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Gaji Pokok Rp";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 266);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Tunjangan Rp";
+            // 
+            // btnhasil
+            // 
+            this.btnhasil.Location = new System.Drawing.Point(80, 191);
+            this.btnhasil.Margin = new System.Windows.Forms.Padding(2);
+            this.btnhasil.Name = "btnhasil";
+            this.btnhasil.Size = new System.Drawing.Size(229, 27);
+            this.btnhasil.TabIndex = 5;
+            this.btnhasil.Text = "Hasil";
+            this.btnhasil.UseVisualStyleBackColor = true;
+            this.btnhasil.Click += new System.EventHandler(this.btnhasil_Click);
+            // 
+            // txtanak
+            // 
+            this.txtanak.Location = new System.Drawing.Point(126, 97);
+            this.txtanak.Margin = new System.Windows.Forms.Padding(2);
+            this.txtanak.Name = "txtanak";
+            this.txtanak.Size = new System.Drawing.Size(145, 20);
+            this.txtanak.TabIndex = 6;
+            this.txtanak.TextChanged += new System.EventHandler(this.txtanak_TextChanged);
+            // 
+            // txtgaji
+            // 
+            this.txtgaji.Location = new System.Drawing.Point(126, 135);
+            this.txtgaji.Margin = new System.Windows.Forms.Padding(2);
+            this.txtgaji.Name = "txtgaji";
+            this.txtgaji.Size = new System.Drawing.Size(145, 20);
+            this.txtgaji.TabIndex = 7;
+            // 
+            // txthasil
+            // 
+            this.txthasil.Location = new System.Drawing.Point(126, 263);
+            this.txthasil.Margin = new System.Windows.Forms.Padding(2);
+            this.txthasil.Name = "txthasil";
+            this.txthasil.Size = new System.Drawing.Size(120, 20);
+            this.txthasil.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(410, 339);
+            this.Controls.Add(this.txthasil);
+            this.Controls.Add(this.txtgaji);
+            this.Controls.Add(this.txtanak);
+            this.Controls.Add(this.btnhasil);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Tunjangan Anak (19SA1138)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbperempuan;
+        private System.Windows.Forms.RadioButton rblaki;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton sudah;
+        private System.Windows.Forms.RadioButton rbbelum;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnhasil;
+        private System.Windows.Forms.TextBox txtanak;
+        private System.Windows.Forms.TextBox txtgaji;
+        private System.Windows.Forms.TextBox txthasil;
+    }
+}
+
---
+++ Praktikum2/Praktikum2/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Praktikum2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtanak.Visible = true;
+            txtanak.Focus();
+            txtgaji.Focus();
+            txthasil.ReadOnly = true;
+        }
+
+        
+
+        private void btnhasil_Click(object sender, EventArgs e)
+        {
+            double hasil = 0, gaji, anak;
+
+            if (rblaki.Checked == true && sudah.Checked == true)
+            {
+                anak = double.Parse(txtanak.Text);
+                gaji = double.Parse(txtgaji.Text);
+                if (anak <= 3 && anak > 0)
+                {
+                    hasil = hasil + 0.20 * anak * gaji;
+                    txthasil.Text = hasil.ToString();
+                }
+                else if (anak>3)
+                {
+                    hasil = hasil + 0.60 * gaji;
+                    txthasil.Text = hasil.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Tidak ada tunjangan");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Tidak ada tunjangan");
+            }
+        }
+
+        private void txtanak_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
